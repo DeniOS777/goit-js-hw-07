0b3d89633ab633f569057a906a5303c3f4e76959
Denis Moskalenko <87602096+DeniOS777@users.noreply.github.com>
2021-12-11 01:37:06 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const divGalleryRef = document.querySelector('.gallery');
+
+const createGalleryImages = array => {
+  return array
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+    })
+    .join('');
+};
+
+const createMarkup = createGalleryImages(galleryItems);
+
+divGalleryRef.insertAdjacentHTML('beforeend', createMarkup);
+
+const instance = basicLightbox.create(`
+    <img  src="#" width="800" height="600">
+`);
+
+const onImageZoom = event => {
+  event.preventDefault();
+  const elemImg = instance.element().children[0].firstChild;
+  console.log(elemImg);
+  const urlBigImage = event.target.dataset.source;
+  const currentElement = event.target.classList.value;
+
+  if (currentElement === 'gallery__image') {
+    elemImg.src = `${urlBigImage}`;
+    instance.show();
+  }
+  return;
+};
+
+divGalleryRef.addEventListener('click', onImageZoom);
